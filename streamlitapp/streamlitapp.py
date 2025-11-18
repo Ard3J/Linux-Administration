c9b2316371fce6fe2562533529ce928fa2fb6cc6
Ard3J <arttujamsa@gmail.com>
2025-11-18 19:43:20 +0000

    18.11 3

--- streamlitapp/streamlitapp.py
+++ streamlitapp/streamlitapp.py
@@ -2,12 +2,20 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+@st.cache_resource
+def mySql():
+	conn = st.connection('mysql', type='sql')
+	df = conn.query('SELECT temp FROM temp;',ttl=600)
+	return df
+
 def main():
-	st.title("Plot some data")
-	df = pd.read_csv("electricity_price.csv")
+	st.title("Kuukauden keskilämpötila, Oulu 2022")
+	st.write("Lämpötila")
+	data = mySql()
 
-	ff = px.scatter(df, x="Date", y="Price (EUR/MWhe)")
-	st.plotly_chart(ff,use_container_width=True)
+	df2 = pd.DataFrame(data, columns=["temp"])
+	temp = px.line(df2, x=df2.index, y="temp")
+	st.plotly_chart(temp, use_container_width=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
